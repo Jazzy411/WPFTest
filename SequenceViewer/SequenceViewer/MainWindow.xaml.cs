1092398212d3eb383cba19c67e622761b7de7c78
Jazzy411 <Jazzy411@126.com>
2020-04-16 13:35:06 +0800

    initialize establish

---
+++ SequenceViewer/SequenceViewer/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MaterialDesignThemes.Wpf;
+
+namespace SequenceViewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            List<Person> l_dataTest = new List<Person>();
+            l_dataTest.Add(new Person("Zhang San","23"));
+            l_dataTest.Add(new Person("Li Si", "25"));
+            l_dataTest.Add(new Person("唐冬梅", "96"));
+
+            lv_data.ItemsSource = l_dataTest;
+        }
+    }
+}
---
+++ SequenceViewer/SequenceViewer/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SequenceViewer
+{
+    class Person
+    {
+        private string _Name;
+        private string _Age;
+        public string Name
+        {
+            get { return _Name; }
+            set { _Name = value; }
+        }
+        public string Age
+        {
+            get { return _Age; }
+            set { _Age = value; }
+        }
+        public Person(string iName, string iAge)
+        {
+            this._Name = iName;
+            this._Age = iAge;
+        }
+    }
+}
